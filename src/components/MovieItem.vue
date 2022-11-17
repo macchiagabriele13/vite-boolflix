d463c14afd07fdd7862ff537da5778191859fcf0
macchiagabriele13 <macchia_gabriele@libero.it>
2022-11-17 15:48:51 +0100

    Commit - Mile 4 Hover Fatto

--- src/components/MovieItem.vue
+++ src/components/MovieItem.vue
@@ -22,15 +22,31 @@ export default {
 
 
     <div class="col">
-        <img :src="`https://image.tmdb.org/t/p/w342${movie.poster_path}`" :alt="movie.title">
-        <h3>{{ movie.title || movie.name }}</h3>
-        <h4>{{ movie.original_title || movie.original_name }}</h4>
-        <h5 v-if="!langList.includes(movie.original_language)">{{ movie.original_language }}</h5>
-        <span v-else class="fi" :class="'fi-' + (movie.original_language)"></span>
 
-        <small>Rating: {{ store.voteStar(movie.vote_average) }} / 5</small>
-        <div class="stars">
-            <i class="fa-solid fa-star" v-for="star in store.voteStar(movie.vote_average)"></i>
+        <div class="card">
+            <div class="copertina">
+                <img :src="`https://image.tmdb.org/t/p/w342${movie.poster_path}`" :alt="movie.title">
+            </div>
+
+            <div class="info">
+                <span> <strong>Titolo:</strong>{{ movie.title || movie.name }}</span>
+                <span><strong>Titolo Originale:</strong>{{ movie.original_title || movie.original_name }}</span>
+                <span class="lingua" v-if="!langList.includes(movie.original_language)"><strong>Lingua:</strong>{{
+                        movie.original_language
+                }}</span>
+                <span v-else class="fi lingua"
+                    :class="'fi-' + (movie.original_language)"><strong>Lingua:</strong></span>
+
+                <!-- <small><strong>Voto:</strong> {{ store.voteStar(movie.vote_average) }} / 5</small> -->
+                <div class="stars">
+                    <strong>Voto:</strong>
+                    <i class="fa-solid fa-star" v-for="star in store.voteStar(movie.vote_average)"></i>
+                </div>
+
+                <p><span class="movie-property"> <strong> OverView: </strong></span> {{ movie.overview }}</p>
+
+            </div>
+
         </div>
 
     </div>
@@ -40,4 +56,47 @@ export default {
 
 <style lang="scss" scoped>
 @import "/node_modules/flag-icons/css/flag-icons.min.css";
+
+.card {
+
+
+    height: 513px;
+    width: 343px;
+    position: relative;
+
+
+    img {
+        width: 100%;
+    }
+
+    .lingua {
+        width: 100%;
+    }
+
+    .info {
+        background-color: rgba(0, 0, 0, 0.495);
+        color: white;
+        position: absolute;
+        width: 100%;
+        height: 100%;
+
+
+
+    }
+}
+
+
+.card:not(:hover)>.info {
+    display: none;
+}
+
+.card:hover>.info {
+    display: flex;
+    flex-direction: column;
+}
+
+.card:hover>.copertina {
+
+    display: none;
+}
 </style>
